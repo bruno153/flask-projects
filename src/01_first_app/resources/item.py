17eee315bbcf2bb36d63e0009b7d04872cfccae1
Bruno <bruno.yamada@supersim.com.br>
2023-08-16 16:20:21 -0300

    Data validation now uses marshmallow

--- src/01_first_app/resources/item.py
+++ src/01_first_app/resources/item.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
@@ -19,10 +20,8 @@ class Item(MethodView):
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found.")
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data
@@ -33,20 +32,9 @@ class Item(MethodView):
 class ItemList(MethodView):
     def get(self):
         return {"items": list(items.values())}
-    def post(self):
-        item_data = request.get_json()
-        # Here not only we need to validate data exists,
-        # But also what type of data. Price should be a float,
-        # for example.
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
